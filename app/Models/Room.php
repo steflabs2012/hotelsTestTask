28f3b7fe8019ebd52cb0583d2e30620cf4346b53
PavelSt <steflabs2012@gmail.com>
2024-12-23 19:53:53 +0400

    Add prices

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -180,7 +180,7 @@ class Room extends Model
 
             // Группируем по board_id
             $groupedByBoard = [];
-            $daysInPeriod = $currentFrom->diffInDays($currentTo) + 1;
+            $daysInPeriod = $currentFrom->diffInDays($currentTo);
 
             foreach ($periodsInRange as $period) {
                 $boardId = $period['board_id'];
